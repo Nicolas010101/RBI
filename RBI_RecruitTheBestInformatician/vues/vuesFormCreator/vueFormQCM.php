0144b4aba12edb2995e00317ee3f4a2bc5d5f780
HugoTAMBERI <hugo.tamberi@free.fr>
2018-12-14 11:31:09 +0100

    modif div info

---
+++ RBI_RecruitTheBestInformatician/vues/vuesFormCreator/vueFormQCM.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HUGO
+ * Date: 14/12/2018
+ * Time: 11:17
+ */
+
+?>
+
+<h1>createur QCM</h1>
+<form method="post">
+    <div class="">
+        <div class="">
+            <span class="" id="" >Nom de la question</span>
+            <input type="text" name="Nom_Question" id="Nom_Question" class ="" />
+        </div>
+    </div><br>
+
+</form>
